262fc2e829d82b34112dd1c1488b06c06dbef1cd
nkzwchen <nkzwchen@outlook.com>
2021-11-30 16:52:56 +0800

    Update Avg.java

--- hadoop/Avg.java
+++ hadoop/Avg.java
@@ -1,16 +1,9 @@
-/**
- * 引用声明
- * 本程序引用自 http://hadoop.apache.org/docs/r1.0.4/cn/mapred_tutorial.html
- */
 package hadoop;
 import java.io.IOException;
 import java.util.*;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
-/**
- * 与 `Map` 相关的方法
- */
 class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
    private Text word = new Text();
    public void map(LongWritable key,
@@ -25,9 +18,6 @@ class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Doub
       output.collect(word, new DoubleWritable(values));
     }
 }
-/**
- * 与 `Reduce` 相关的方法
- */
 class Reduce extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
    public void reduce(Text key,
                   Iterator<DoubleWritable> values,
@@ -55,9 +45,7 @@ public class Avg {
       conf.setReducerClass(Reduce.class);
       conf.setInputFormat(TextInputFormat.class);
       conf.setOutputFormat(TextOutputFormat.class);
-      // 第一个参数表示输入
       FileInputFormat.setInputPaths(conf, new Path(args[0]));
-      // 第二个输入参数表示输出
       FileOutputFormat.setOutputPath(conf, new Path(args[1]));
       JobClient.runJob(conf);
    }
